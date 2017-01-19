7c2915895c999123218f3888d48cb13a2a473c5b
Jeric Brual <jbrual@ucvts.org>
2017-01-19 16:11:09 +0000

    Removed old Pokemon.py.

--- Pokemon.py
+++ Pokemon.py
@@ -1,6 +1,8 @@
-# Q: What are the ascending speeds of a given type of Pokemon?
+# Q: What are the stats of a given type of Pokemon?
 # Import the appropriate modules.
-import csv, requests, operator
+import csv, requests, operator, pandas as pd, numpy as np
+from bokeh.plotting import figure, output_file, save
+from bokeh.charts import Bar, output_file, save
 
 # Reach the API by building its URL.
 # Use user input to ask for a certain type.
@@ -14,32 +16,52 @@ results = r.json()
 type_list = results["pokemon"]
 
 # Create a .csv file for export data.
-csvfile = open("Speeds.csv", "w")
+csvfile = open("Stats.csv", "w")
 csvwriter = csv.writer(csvfile, delimiter = ",")
 
 # Establish header row.
-csvwriter.writerow(["Pokemon", "Speed"])
+csvwriter.writerow(["Pokemon", "HP", "Atk", "Def", "SAtk", "SDef", "Spd"])
 
-# Create an empty dictionary to sort by values later on.
-speed_pokemon = {}
+# Create empty lists to sort by values later on.
+name_pokemon = []
+hp_pokemon = []
+atk_pokemon = []
+def_pokemon = []
+satk_pokemon = []
+sdef_pokemon = []
+speed_pokemon = []
 
-# For each Pokemon, determine its name and base speed stat.
-# Export them into a dictionary as a list.
+# For each Pokemon, determine its name and stats.
+# Append them to their respective lists.
 for pokemon in type_list:
     name = pokemon["pokemon"]["name"]
     url = pokemon["pokemon"]["url"]
     rspeed = requests.get(url)
-    speed_results = rspeed.json()
-    speed_list = speed_results
-    speed = speed_list["stats"][0]["base_stat"]
-    speed_pokemon[name] = speed
+    results = rspeed.json()
+    hp = results["stats"][5]["base_stat"]
+    atk = results["stats"][4]["base_stat"]
+    defense = results["stats"][3]["base_stat"]
+    satk = results["stats"][2]["base_stat"]
+    sdef = results["stats"][1]["base_stat"]
+    speed = results["stats"][0]["base_stat"]
+    
+    name_pokemon.append(name)
+    hp_pokemon.append(hp)
+    atk_pokemon.append(atk)
+    def_pokemon.append(defense)
+    satk_pokemon.append(satk)
+    sdef_pokemon.append(sdef)
+    speed_pokemon.append(speed)
 
-# Sort by dictionary values.
-sortedlist = sorted(speed_pokemon.items(), key = operator.itemgetter(1))
-
-# For each pokemon in the sorted list, write a row in the .csv file.
-for pokemon in sortedlist:
-    csvwriter.writerow(pokemon)
+rows = zip(name_pokemon,hp_pokemon,atk_pokemon,def_pokemon,satk_pokemon,sdef_pokemon,speed_pokemon)
+for row in rows:
+    csvwriter.writerow(row)
 
 # Close the .csv file.
 csvfile.close()
+
+df = pd.read_csv("Stats.csv")
+graph1 = Bar(df, 'Pokemon', values='Spd', legend=False, title= type_poke + "-type Pokemon Stats")
+
+output_file("Stats.html")
+save(graph1)
